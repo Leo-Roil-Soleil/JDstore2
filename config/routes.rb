eddcbeebb2d2facc1e7f5c7b0d13c30bf22a2b5c
Leo-Roil-Soleil <leokexin@gmail.com>
2017-04-17 21:52:11 +0800

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'products#index'
     resources :products
+    resources :cart_items
 
     resources :carts do
         collection do
