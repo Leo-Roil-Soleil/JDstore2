4fa293b711f54a4f7e77fecdde06e43cd3686439
Leo-Roil-Soleil <leokexin@gmail.com>
2017-04-17 21:48:41 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
     namespace :admin do
-    resources :products
-
+        resources :products
     end
 
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'products#index'
     resources :products
-    resources :carts
-      resources :products do
-    member do
-      post :add_to_cart
+
+    resources :carts do
+        collection do
+            delete :clean
+        end
     end
-  end
+    resources :products do
+        member do
+            post :add_to_cart
+        end
+    end
 end
